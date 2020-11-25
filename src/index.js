bcb8769a2e09d838e37712261cb0648b228d8d15
Alexandre Gérault <alexandre@hexium.io>
2020-11-25 23:02:58 +0100

    refactor: display DataTable using fetching

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React, {useEffect, useState} from 'react'
 import ReactDOM from 'react-dom'
 import DataTable from './components/DataTable/DataTable'
+import {fetchLaravelData} from './functions/fetchLaravelData'
 import './tailwind.output.css'
 
 // http://127.0.0.1:8000/api/user
 function App() {
   return (
     <DataTable
-      endpoint="http://127.0.0.1:8000/api/user"
-      perPage={3}
+      fetcher={fetchLaravelData}
+      endpoint="http://127.0.0.1:8000/api/user" // Use a fetcher function to make it more abstract and flexible
+      perPage={5}
       columns={[
         {label: 'Prénom', name: 'firstname'},
         {label: 'Nom', name: 'lastname'},
