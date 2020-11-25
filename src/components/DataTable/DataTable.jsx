f3bbee6f030a9f8375d9047e0175f6ac858fb9c3
Alexandre Gérault <alexandre@hexium.io>
2020-11-25 22:45:50 +0100

    refactor(DataTable): make component backend framework agnostic
    Use a fetcher as prop

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -7,26 +7,24 @@ import CellHeader from '../Table/CellHeader'
 import TableCell from '../Table/TableCell'
 import Pagination from '../Pagination/Pagination'
 
-const DataTable = ({columns, endpoint, perPage}) => {
+const DataTable = ({columns, endpoint, perPage, fetcher}) => {
   const [isLoaded, setIsLoaded] = useState(false)
-  const [lastPage, setLastPage] = useState(0)
+  const [meta, setMeta] = useState({})
   const [page, prevPage, nextPage, goToPage] = usePagination({
-    initialPage: 1,
-    maxPages: lastPage,
+    maxPages: meta.lastPage,
   })
   const [data, setData] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(`${endpoint}?page=${page}&perPage=${perPage}`)
-      const json = await res.json()
-      console.log(json)
-      setData(json.data)
-      setLastPage(json.meta.last_page)
+      setIsLoaded(false)
+      const {data, meta} = await fetcher(endpoint, page, perPage)
+      setData(data)
+      setMeta(meta)
       setIsLoaded(true)
     }
     fetchData()
-  }, [page, endpoint, perPage])
+  }, [page, endpoint, perPage, fetcher])
 
   return (
     <div className="p-16">
@@ -34,13 +32,18 @@ const DataTable = ({columns, endpoint, perPage}) => {
         <Table
           footer={
             <div className="flex justify-between p-4 border-t items-center">
-              <p>This is page {page}</p>
+              <div className="flex flex-col">
+                <p>
+                  Résultats {meta.from} à {meta.to}
+                </p>
+                <p>Total : {meta.total} résultat </p>
+              </div>
               <Pagination
                 page={page}
                 prevPage={prevPage}
                 nextPage={nextPage}
                 goToPage={goToPage}
-                last={lastPage}
+                last={meta.lastPage}
               />
             </div>
           }
@@ -54,14 +57,36 @@ const DataTable = ({columns, endpoint, perPage}) => {
             {data.map((item, index) => (
               <tr key={index}>
                 {columns.map((column, index) => (
-                  <TableCell>{item[column.name]}</TableCell>
+                  <TableCell key={item[column.name]}>
+                    {item[column.name]}
+                  </TableCell>
                 ))}
               </tr>
             ))}
           </TableBody>
         </Table>
       ) : (
-        'Chargement... '
+        <button type="button" className="bg-rose-600 ..." disabled>
+          <svg version="1.0" width="64px" height="64px" viewBox="0 0 128 128">
+            <rect x="0" y="0" width="100%" height="100%" fill="#FFFFFF" />
+            <g>
+              <path
+                d="M75.4 126.63a11.43 11.43 0 0 1-2.1-22.65 40.9 40.9 0 0 0 30.5-30.6 11.4 11.4 0 1 1 22.27 4.87h.02a63.77 63.77 0 0 1-47.8 48.05v-.02a11.38 11.38 0 0 1-2.93.37z"
+                fill="#000000"
+                fillOpacity="1"
+              />
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 64 64"
+                to="360 64 64"
+                dur="1800ms"
+                repeatCount="indefinite"
+              ></animateTransform>
+            </g>
+          </svg>
+          Processing
+        </button>
       )}
     </div>
   )
